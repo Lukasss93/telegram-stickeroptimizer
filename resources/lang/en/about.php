1d1f56f54405251e0835b74331b68dd1866807c5
Luca Patera <lucapatera@outlook.it>
2021-03-04 21:26:15 +0100

    Initialized localization files

---
+++ resources/lang/en/about.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'bot' => '🤖 Bot',
+    'name' => '<b>Name:</b> :value',
+    'username' => '<b>Username:</b> :value',
+    'version' => '<b>Version:</b> :value',
+    'source' => '<b>Source code:</b> :value',
+    'changelog' => '<b>Changelog:</b> :value',
+    'developer' => '👤 Developer',
+    'email' => '<b>Email:</b> :value',
+    'website' => '<b>Website:</b> :value',
+
+];
---
+++ resources/lang/en/start.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'description' => 'Just send me an image or sticker, and I will convert it to a png file and optimized it to make its size smaller than or equal to 350Kb, so that you will be able to add it to a sticker pack using the @stickers bot.',
+
+];
