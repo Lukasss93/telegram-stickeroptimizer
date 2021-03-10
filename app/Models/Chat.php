fcbe6f2b8f3023573498d3dfca6c4121974477da
Luca Patera <lucapatera@outlook.it>
2021-03-10 01:20:31 +0100

    Added chat + job migrations Removed unused migrations

---
+++ app/Models/Chat.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
+use SergiX44\Nutgram\Telegram\Types\User;
+
+/**
+ * App\Models\Chat
+ *
+ * @property int $chat_id
+ * @property string $type
+ * @property string $first_name
+ * @property string|null $last_name
+ * @property string|null $username
+ * @property string|null $language_code
+ * @property bool $status
+ * @property bool $blocked
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @method static Builder|Chat newModelQuery()
+ * @method static Builder|Chat newQuery()
+ * @method static Builder|Chat query()
+ * @method static Builder|Chat whereBlocked($value)
+ * @method static Builder|Chat whereChatId($value)
+ * @method static Builder|Chat whereCreatedAt($value)
+ * @method static Builder|Chat whereFirstName($value)
+ * @method static Builder|Chat whereLanguageCode($value)
+ * @method static Builder|Chat whereLastName($value)
+ * @method static Builder|Chat whereStatus($value)
+ * @method static Builder|Chat whereType($value)
+ * @method static Builder|Chat whereUpdatedAt($value)
+ * @method static Builder|Chat whereUsername($value)
+ * @mixin Eloquent
+ */
+class Chat extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'chat_id';
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $guarded = [];
+    protected $casts = [
+        'status' => 'boolean',
+        'blocked' => 'boolean',
+    ];
+
+    public static function findFromUser(?User $user): ?Chat
+    {
+        if ($user === null) {
+            return null;
+        }
+
+        $chat = self::find($user->id);
+
+        return $chat ?? null;
+    }
+}
---
+++ database/migrations/2021_03_04_210800_create_chats_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('chats', function (Blueprint $table) {
+            $table->bigInteger('chat_id')->primary();
+            $table->string('type');
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('username')->nullable();
+            $table->string('language_code')->nullable();
+            $table->boolean('status')->default(false);
+            $table->boolean('blocked')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('chats');
+    }
+}
