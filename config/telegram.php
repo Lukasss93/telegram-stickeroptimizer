341d6ad796109d2c06c8a474c0d5bcdf69990216
Luca Patera <lucapatera@outlook.it>
2021-03-12 20:32:29 +0100

    Refactored telegram config

--- config/telegram.php
+++ config/telegram.php
@@ -2,34 +2,25 @@
 
 return [
 
+    'dev' => [
+        'id' => (int) env('DEV_ID'),
+        'name' => env('DEV_NAME'),
+        'username' => env('DEV_USERNAME'),
+        'email' => env('DEV_EMAIL'),
+        'website' => env('DEV_WEBSITE'),
+    ],
+
     'bot' => [
         'name' => env('BOT_NAME', env('APP_NAME', 'Telegram Bot')),
-
         'username' => env('BOT_USERNAME'),
-
         'version' => env('BOT_VERSION'),
-
         'source' => env('BOT_SOURCE'),
-
         'changelog' => env('BOT_CHANGELOG'),
-
         'commands' => [
             ['command' => 'start', 'description' => 'Welcome message'],
             ['command' => 'help', 'description' => 'Help message'],
             ['command' => 'about', 'description' => 'About the bots'],
         ],
-    ],
-
-    'developer' => [
-        'id' => (int) env('DEV_ID'),
-
-        'name' => env('DEV_NAME'),
-
-        'username' => env('DEV_USERNAME'),
-
-        'email' => env('DEV_EMAIL'),
-
-        'website' => env('DEV_WEBSITE'),
     ],
 
 ];
--- resources/views/messages/about.blade.php
+++ resources/views/messages/about.blade.php
@@ -4,16 +4,16 @@
 @lang('about.version',['value'=>config('telegram.bot.version')])<br>
 
 @if(config('telegram.bot.source'))
-@lang('about.source',['value'=>'<a href="'.config('telegram.bot.source').'">Github</a>'])<br>
+    @lang('about.source',['value'=>'<a href="'.config('telegram.bot.source').'">Github</a>'])<br>
 @endif
 
 @if(config('telegram.bot.changelog'))
-@lang('about.changelog',['value'=>'<a href="'.config('telegram.bot.changelog').'">Telegraph</a>'])<br>
+    @lang('about.changelog',['value'=>'<a href="'.config('telegram.bot.changelog').'">Telegraph</a>'])<br>
 @endif
 <br>
 
 <b>@lang('about.developer')</b><br>
-@lang('about.name',['value'=>config('telegram.developer.name')])<br>
-@lang('about.username',['value'=>config('telegram.developer.username')])<br>
-@lang('about.email',['value'=>config('telegram.developer.email')])<br>
-@lang('about.website',['value'=>config('telegram.developer.website')])<br>
+@lang('about.name',['value'=>config('telegram.dev.name')])<br>
+@lang('about.username',['value'=>config('telegram.dev.username')])<br>
+@lang('about.email',['value'=>config('telegram.dev.email')])<br>
+@lang('about.website',['value'=>config('telegram.dev.website')])<br>
