e563642f1db9947a0ddf6d892dc9fc2ec41e5cfa
Luca Patera <lucapatera@outlook.it>
2021-05-17 01:40:37 +0200

    The saved migrations are now anonymous

--- database/migrations/2021_03_04_210800_create_chats_table.php
+++ database/migrations/2021_03_04_210800_create_chats_table.php
@@ -4,13 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateChatsTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('chats', function (Blueprint $table) {
@@ -26,13 +20,8 @@ class CreateChatsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down(): void
     {
         Schema::dropIfExists('chats');
     }
-}
+};
