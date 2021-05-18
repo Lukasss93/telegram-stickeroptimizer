77e24ac0f849e53885484388710e01b517f3f27b
Luca Patera <lucapatera@outlook.it>
2021-05-18 02:26:11 +0200

    Added statistics strings

---
+++ resources/lang/en/stats.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'title' => '<b>📊 Statistics</b>',
+    'stickers' => [
+        'today' => '<b>🆕 Stickers optimized today:</b> :value',
+        'total' => '<b>🖼 Total stickers optimized:</b> :value',
+    ],
+    'users' => [
+        'today' => [
+            'new' => '<b>🆕 New users today:</b> :value',
+            'active' => '<b>✳️ Active users today:</b> :value',
+        ],
+        'total' => '<b>👥 Total users:</b> :value',
+    ],
+    'last_update' => '<i>Last update:</i> :value',
+
+];
---
+++ resources/lang/it/stats.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    'title' => '<b>📊 Statistiche</b>',
+    'stickers' => [
+        'today' => '<b>🆕 Sticker ottimizzati oggi:</b> :value',
+        'total' => '<b>🖼 Totale sticker ottimizzati:</b> :value',
+    ],
+    'users' => [
+        'today' => [
+            'new' => '<b>🆕 Nuovi utenti oggi:</b> :value',
+            'active' => '<b>✳️ Utenti attivi oggi:</b> :value',
+        ],
+        'total' => '<b>👥 Utenti totali:</b> :value',
+    ],
+    'last_update' => '<i>Ultimo aggiornamento:</i> :value',
+
+];
