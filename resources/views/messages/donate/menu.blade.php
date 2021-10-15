aac73b530d7942853a25dd1b558917f06cd6d847
Luca Patera <lucapatera@outlook.it>
2021-10-16 01:46:25 +0200

    Fix .editorconfig

--- resources/views/messages/donate/menu.blade.php
+++ resources/views/messages/donate/menu.blade.php
@@ -1,7 +1,7 @@
 @lang('donate.consider_donation')<br>
 @lang('donate.appreciated')<br>
 @if(!empty(config('bot.donations.terms')))
-    <br>
-    <a href="{{ config('bot.donations.terms') }}">@lang('donate.terms')</a>
+<br>
+<a href="{{ config('bot.donations.terms') }}">@lang('donate.terms')</a>
 @endif
 
