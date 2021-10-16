c007a4cc0fe43e7daeeb12bd73616f97d7562671
Luca Patera <lucapatera@outlook.it>
2021-10-16 15:31:32 +0200

    Fix typo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //update the bot stattistics
+        //update the bot statistics
         $schedule
             ->command(UpdateBotStatsCommand::class)
             ->everyFiveMinutes();
