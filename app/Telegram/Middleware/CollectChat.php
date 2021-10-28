3136ad33f7464452261ff26e65944c44e0e56b72
Luca Patera <lucapatera@outlook.it>
2021-10-29 01:58:50 +0200

    Removed type column from chats table

--- app/Telegram/Middleware/CollectChat.php
+++ app/Telegram/Middleware/CollectChat.php
@@ -5,7 +5,6 @@ namespace App\Telegram\Middleware;
 use App\Models\Chat;
 use Illuminate\Support\Facades\DB;
 use SergiX44\Nutgram\Nutgram;
-use SergiX44\Nutgram\Telegram\Attributes\UpdateTypes;
 
 class CollectChat
 {
@@ -19,23 +18,10 @@ class CollectChat
 
         $chat = DB::transaction(function () use ($user, $bot) {
 
-            //get chat type
-            $type = match ($bot->update()?->getType()) {
-                UpdateTypes::MESSAGE => $bot->update()->message->chat->type,
-                UpdateTypes::EDITED_MESSAGE => $bot->update()->edited_message->chat->type,
-                UpdateTypes::INLINE_QUERY, UpdateTypes::CALLBACK_QUERY => 'private',
-                default => 'unknown',
-            };
-
-            if ($type === 'unknown') {
-                info('unknown', [$bot->update()]);
-            }
-
             //save or update chat
             $chat = Chat::updateOrCreate([
                 'chat_id' => $user->id,
             ], [
-                'type' => $type,
                 'first_name' => $user->first_name,
                 'last_name' => $user->last_name,
                 'username' => $user->username,
