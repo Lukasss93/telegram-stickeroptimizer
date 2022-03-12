aae1039e3d8532ffd58e252acbc06f8960fdb7f4
Luca Patera <lucapatera@outlook.it>
2022-03-12 02:52:51 +0100

    Added CheckMaintenance test

---
+++ tests/Feature/Middleware/CheckMaintenanceTest.php
@@ -0,0 +1,65 @@
+<?php
+
+use App\Models\Chat;
+use App\Telegram\Middleware\CheckMaintenance;
+use SergiX44\Nutgram\Nutgram;
+
+beforeEach(function () {
+    $this->artisan('down');
+
+    bot()->overrideMiddleware(CheckMaintenance::class);
+    bot()->onText('foo', fn (Nutgram $bot) => $bot->sendMessage('bar'));
+    bot()->onCallbackQueryData('aaa', fn (Nutgram $bot) => $bot->sendMessage('bbb'));
+});
+
+afterEach(function () {
+    $this->artisan('up');
+});
+
+it('checks maintenance is off', function () {
+    $this->artisan('up');
+
+    bot()
+        ->hearText('foo')
+        ->reply()
+        ->assertReplyText('bar');
+});
+
+it('checks maintenance is on', function () {
+    $chat = Chat::factory()->create();
+    bot()->setData(Chat::class, $chat);
+
+    bot()
+        ->hearText('foo')
+        ->reply()
+        ->assertReplyText(message('maintenance'));
+});
+
+it('checks maintenance is on but user is developer', function () {
+    config()->set('developer.id', 123456789);
+    $chat = Chat::factory()->create(['chat_id' => 123456789]);
+    bot()->setData(Chat::class, $chat);
+
+    bot()
+        ->hearMessage([
+            'chat' => ['id' => 123456789],
+            'from' => ['id' => 123456789],
+            'text' => 'foo',
+        ])
+        ->reply()
+        ->assertReplyText('<b>⚠ MAINTENANCE MODE ENABLED ⚠</b>')
+        ->assertReplyText('bar', 1);
+});
+
+it('checks maintenance is on but bot hears a callback query', function () {
+    $chat = Chat::factory()->create();
+    bot()->setData(Chat::class, $chat);
+
+    bot()
+        ->hearCallbackQueryData('aaa')
+        ->reply()
+        ->assertReply('answerCallbackQuery', [
+            'text' => trans('maintenance.title'),
+            'show_alert' => true,
+        ]);
+});
