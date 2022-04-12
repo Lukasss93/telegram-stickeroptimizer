9309639a883155f35fc66bff8592384a2e718b75
Luca Patera <lucapatera@outlook.it>
2022-04-12 21:36:08 +0200

    Fix wrong message with video stickers

--- app/Telegram/Handlers/StickerHandler.php
+++ app/Telegram/Handlers/StickerHandler.php
@@ -9,11 +9,13 @@ class StickerHandler
 {
     public function __invoke(Nutgram $bot)
     {
-        $replyID = $bot->messageId();
-        $fileSize = $bot->message()->sticker->file_size;
-        $fileID = $bot->message()->sticker->file_id;
+        $sticker = $bot->message()->sticker;
 
-        if ($bot->message()->sticker->is_animated) {
+        $replyID = $bot->messageId();
+        $fileSize = $sticker->file_size;
+        $fileID = $sticker->file_id;
+
+        if ($sticker->is_animated || $sticker->is_video) {
             $bot->sendMessage(trans('common.animated_not_supported'), [
                 'reply_to_message_id' => $replyID,
                 'allow_sending_without_reply' => true,
